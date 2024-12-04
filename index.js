be2e1c9972e84b53f94545ee665173ac2f0e9eb7
winsep tran <winsep2004@gmail.com>
2024-12-04 16:50:45 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/tin-tuc', (req, res) => {
+  var a = 1;
+  var b = 2;
+
+  var c = a + b;
+  res.send('Hello Winsep!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
